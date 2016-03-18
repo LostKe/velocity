5df78da264d2b93bb328a6b2a148bae1b6be74c7
zhangshuqing <shu_qingzhang@126.com>
2016-03-18 16:47:37 +0800

    add

--- src/main/java/com/guogee/action/ShowAction.java
+++ src/main/java/com/guogee/action/ShowAction.java
@@ -1,16 +1,17 @@
 package com.guogee.action;
 
+import com.guogee.exception.BussinessException;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class ShowAction extends ActionSupport{
 	
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 1L;
 
 	public String showMsg() throws Exception{
 		System.out.println("enter showMsg");
-		throw new RuntimeException("define Exception");
+		throw new BussinessException("define Exception");
 	}
+	
+	
 }
